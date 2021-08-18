2b8a0fdf2797de8cf6f2ee9b7c8ff82b2bd0168e
n-y-kim <nyn2265@gmail.com>
2021-08-19 01:28:26 +0900

    installed cocoapod for bottom sheet

---
+++ BottomSheetTest/CocoapodBottom/CBTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CBTableViewController.swift
+//  BottomSheetTest
+//
+//  Created by Nayeon Kim on 2021/08/19.
+//
+
+import UIKit
+
+class CBTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BottomSheetTest/CocoapodBottom/CBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CBViewController.swift
+//  BottomSheetTest
+//
+//  Created by Nayeon Kim on 2021/08/19.
+//
+
+import UIKit
+
+class CBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
